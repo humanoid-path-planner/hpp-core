3a112d0939fb5ab011b59b7efbfe4ad3ea5ff9cd
Joseph Mirabel <jmirabel@laas.fr>
2014-09-12 11:10:50 +0200

    Update CMakeLists.txt and add inequality.cc

---
+++ src/inequality.cc
@@ -0,0 +1,23 @@
+// Copyright (c) 2014, LAAS-CNRS
+// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+//
+// This file is part of hpp-core.
+// hpp-core is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-core is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-core. If not, see <http://www.gnu.org/licenses/>.
+
+#include "hpp/core/inequality.hh"
+
+namespace hpp {
+  namespace core {
+    EqualityPtr_t Equality::unique_ = Equality::create ();
+  } // namespace core
+} // namespace hpp
