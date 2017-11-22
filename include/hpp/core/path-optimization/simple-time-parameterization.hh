d6bf79fcf8244755e5a9ea0cd3ce895990f280f7
Joseph Mirabel <jmirabel@laas.fr>
2017-11-22 13:45:24 +0100

    Add formula of SimpleTimeParameterization in the documentation

--- include/hpp/core/path-optimization/simple-time-parameterization.hh
+++ include/hpp/core/path-optimization/simple-time-parameterization.hh
@@ -25,11 +25,63 @@ namespace hpp {
       /// \addtogroup path_optimization
       /// \{
 
-      /// Change the time of straight paths so that the velocity does not
-      /// exceeds the velocity limits.
-      ///
-      /// Parameter SimpleTimeParameterization/Safety (value_type) defines
-      /// is used to rescale the velocity limit.
+      /** Add a TimeParameterization to paths so that the velocity does not
+       *  exceeds the velocity limits.
+       * 
+       *  Parameter SimpleTimeParameterization/safety (value_type) defines
+       *  is used to rescale the velocity limit.
+       * 
+       *  Parameter SimpleTimeParameterization/velocity (bool) defines
+       *  whether to use first or third order Polynomial.
+       * 
+       *  Let the initial path be
+       *  \f[ \begin{align*}
+       *    q \colon  [s_0, s_1] & \to \mathcal{C} \\
+       *               s         & \mapsto q(s)
+       *  \end{align*} \f]
+       *  We want to find a increasing Polynomial
+       *  \f[ \begin{align*}
+       *    P_n \colon  [0, T] & \to [s_0, s_1] \\
+       *                 t     & \mapsto s
+       *  \end{align*} \f]
+       *  where \f$n\f$ is the polynom degree.
+       *  The time parameterized path should satisfy the velocity limits so:
+       *  \f[
+       *    l_i \le \dot q_i (P_n(t)) = P'_n(t) \frac{dq_i }{ds} (P_n(t)) \le u_i \\
+       *    P'_n(t) | \frac{dq_i }{ds} (P_n(t)) | \le \min u_i, -l_i \\
+       *  \f]
+       *  where \f$ u_i > 0 \f$ and \f$ l_i < 0 \f$ are the velocity limits.
+       * 
+       *  Let \f$ v \f$ be a velocity bound (Path::velocityBound) of \f$ q \f$ on \f$ [s_0, s_1] \f$.
+       *  Then \f$ P'_n(t) \le B \f$ where
+       *  \f[
+       *   B = \min_i \frac{u_i}{v_i}, \frac{-l_i}{v_i}
+       *  \f]
+       * 
+       *  The constraints on \f$ P_n \f$ are:
+       *  - \f$ P_n(0) = s_0 \f$
+       *  - \f$ P_n(T) = s_1 \f$
+       *  - \f$ P'_n(t) \le B \f$
+       *  - \f$ P'_n(0) = 0 \f$ (in the third order case only)
+       *  - \f$ P'_n(T) = 0 \f$ (in the third order case only)
+       * 
+       *  The solutions are:
+       *  \li First order case:
+       *    \f[ \begin{align*}
+       *    a_0 &= s_0              \\
+       *    a_1 &= B                \\
+       *    T &= \frac{s_1 - s_0}{B} \\
+       *    \end{align*} \f]
+       * 
+       *  \li Third order case:
+       *    \f[ \begin{align*}
+       *    T &= 3 \frac{s_1 - s_0}{2 B} \\
+       *    a_0 &= s_0              \\
+       *    a_1 &= 0                \\
+       *    a_2 &= 3 \frac{s_1 - s_0}{T^2} \\
+       *    a_3 &= - \frac{2 a_2}{3 T} \\
+       *    \end{align*} \f]
+       */
 
       class HPP_CORE_DLLAPI SimpleTimeParameterization : public PathOptimizer
       {
