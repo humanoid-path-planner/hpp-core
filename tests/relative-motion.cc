3e9a7b0de194e56c9171f02f7d231e78d13c5cbd
Joseph Mirabel <jmirabel@laas.fr>
2017-11-22 16:52:52 +0100

    Remove unnecessary comment

--- tests/relative-motion.cc
+++ tests/relative-motion.cc
@@ -14,22 +14,6 @@
 // received a copy of the GNU Lesser General Public License along with
 // hpp-core. If not, see <http://www.gnu.org/licenses/>.
 
-// Copyright (c) 2016, Joseph Mirabel
-// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
-//
-// This file is part of hpp-core.
-// hpp-core is free software: you can redistribute it
-// and/or modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation, either version
-// 3 of the License, or (at your option) any later version.
-//
-// hpp-core is distributed in the hope that it will be
-// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Lesser Public License for more details.  You should have
-// received a copy of the GNU Lesser General Public License along with
-// hpp-core. If not, see <http://www.gnu.org/licenses/>.
-
 #define BOOST_TEST_MODULE relativeMotion
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/test_case_template.hpp>
