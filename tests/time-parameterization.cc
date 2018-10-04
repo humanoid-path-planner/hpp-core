7757bbc2f683d669c8beb52b1449ba2b76ad2e3d
Joseph Mirabel <jmirabel@laas.fr>
2018-10-04 20:29:04 +0200

    Add unit test for time-parameterization

--- tests/time-parameterization.cc
+++ tests/time-parameterization.cc
@@ -90,4 +90,37 @@ BOOST_AUTO_TEST_CASE (cubic2)
   BOOST_CHECK_EQUAL(P.derivativeBound(1./3,1), 4./3);
 }
 
+value_type integrate (const timeParameterization::Polynomial& P,
+    const value_type& L)
+{
+  int N = 10000;
+
+  value_type dt = L / N;
+  value_type integral = 0;
+
+  for (int i = 0; i < N; ++i)
+  {
+    integral += P.derivative (i*dt, 1) * dt;
+  }
+  return integral;
+}
+
+BOOST_AUTO_TEST_CASE (degree5integration)
+{
+  value_type prec = 0.01; // %
+  vector_t a (6);
+  {
+    a << 0, 0, 0, 2.10515, -3.83792, 1.86585;
+    timeParameterization::Polynomial P (a);
+    value_type integral = integrate (P, 0.822769);
+    BOOST_CHECK_CLOSE (integral, 0.117251, prec);
+  }
+  {
+    a << 0, 0, 0, 1.13097, -1.10773, 0.289323;
+    timeParameterization::Polynomial P (a);
+    value_type integral = integrate (P, 1.53147);
+    BOOST_CHECK_CLOSE (integral, 0.406237, prec);
+  }
+}
+
 BOOST_AUTO_TEST_SUITE_END ()
