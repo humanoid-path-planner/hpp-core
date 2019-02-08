2ae26c3018649ead837666e05ae3d34395fbe113
Joseph Mirabel <jmirabel@laas.fr>
2019-02-08 13:42:18 +0100

    Minor fix
    Fixes #127.

--- src/steering-method/spline.cc
+++ src/steering-method/spline.cc
@@ -84,18 +84,18 @@ namespace hpp {
         // Compute the matrices
         // TODO calls to basisFunctionDerivative could be cached as they do not
         // depend on the inputs.
-        SplinePath::timeFreeBasisFunctionDerivative(0, 0, coeffs.row(0));
+        SplinePath::timeFreeBasisFunctionDerivative(0, 0, coeffs.row(0).transpose());
         pinocchio::difference<pinocchio::RnxSOnLieGroupMap>(device_.lock(), q1, p->base(), rhs.row(0));
         for (std::size_t i = 0; i < order1.size(); ++i)
-          SplinePath::timeFreeBasisFunctionDerivative(order1[i], 0, coeffs.row(i+1));
+          SplinePath::timeFreeBasisFunctionDerivative(order1[i], 0, coeffs.row(i+1).transpose());
         rhs.middleRows(1, order1.size()).transpose() = derivatives1;
 
         size_type row = 1 + order1.size();
-        SplinePath::timeFreeBasisFunctionDerivative(0, 1, coeffs.row(row));
+        SplinePath::timeFreeBasisFunctionDerivative(0, 1, coeffs.row(row).transpose());
         pinocchio::difference<pinocchio::RnxSOnLieGroupMap>(device_.lock(), q2, p->base(), rhs.row(row));
         ++row;
         for (std::size_t i = 0; i < order2.size(); ++i)
-          SplinePath::timeFreeBasisFunctionDerivative(order2[i], 1, coeffs.row(i+row));
+          SplinePath::timeFreeBasisFunctionDerivative(order2[i], 1, coeffs.row(i+row).transpose());
         rhs.middleRows(row, order2.size()).transpose() = derivatives2;
 
         // Solve the problem
