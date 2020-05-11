ea75ffcd385e08979c5f5744ee658c6ad65fd7e2
Joseph Mirabel <jmirabel@laas.fr>
2020-05-11 19:45:00 +0200

    Serialization of ConnectedComponent

--- src/serialization.cc
+++ src/serialization.cc
@@ -22,6 +22,7 @@
 
 #include <pinocchio/serialization/eigen.hpp>
 
+#include <hpp/core/connected-component.hh>
 #include <hpp/core/edge.hh>
 #include <hpp/core/node.hh>
 
@@ -29,6 +30,7 @@
 
 BOOST_CLASS_EXPORT(hpp::core::Node)
 BOOST_CLASS_EXPORT(hpp::core::Edge)
+BOOST_CLASS_EXPORT(hpp::core::ConnectedComponent)
 
 namespace hpp {
 namespace core {
@@ -53,6 +55,17 @@ inline void Edge::serialize(Archive& ar, const unsigned int version)
   //ar & BOOST_SERIALIZATION_NVP(path_);
 }
 HPP_SERIALIZATION_IMPLEMENT(Edge);
+
+template <typename Archive>
+inline void ConnectedComponent::serialize(Archive& ar, const unsigned int version)
+{
+  (void) version;
+  ar & BOOST_SERIALIZATION_NVP(nodes_);
+  ar & BOOST_SERIALIZATION_NVP(reachableFrom_);
+  ar & BOOST_SERIALIZATION_NVP(reachableTo_);
+  ar & BOOST_SERIALIZATION_NVP(weak_);
+}
+HPP_SERIALIZATION_IMPLEMENT(ConnectedComponent);
 
 } //   namespace core
 } // namespace hpp
