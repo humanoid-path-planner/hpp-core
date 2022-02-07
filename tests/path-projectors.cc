bc783d29e79456d73f72421d89b33f464f53e58a
Florent Lamiraux <florent@laas.fr>
2022-02-07 10:07:18 +0000

    [tests] Replace deprecated header.

--- tests/path-projectors.cc
+++ tests/path-projectors.cc
@@ -17,7 +17,7 @@
 #define BOOST_TEST_MODULE pathProjector
 #include <pinocchio/fwd.hpp>
 #include <boost/test/included/unit_test.hpp>
-#include <boost/test/test_case_template.hpp>
+#include <boost/test/unit_test.hpp>
 #include <boost/mpl/list.hpp>
 
 #include <boost/shared_ptr.hpp>
