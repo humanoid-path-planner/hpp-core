2296fcb3e7dde61f2c7919a1bc821a98b487e9cb
Diane Bury <dbury@eurekarobotics.com>
2023-02-22 15:17:45 +0100

    fix spline steering method when path length != 1

--- src/steering-method/spline.cc
+++ src/steering-method/spline.cc
@@ -107,22 +107,22 @@ PathPtr_t Spline<_PB, _SO>::impl_compute(
   // Compute the matrices
   // TODO calls to basisFunctionDerivative could be cached as they do not
   // depend on the inputs.
-  SplinePath::timeFreeBasisFunctionDerivative(0, 0, coeffs.row(0).transpose());
+  p->basisFunctionDerivative(0, 0, coeffs.row(0).transpose());
   pinocchio::difference<pinocchio::RnxSOnLieGroupMap>(device_.lock(), q1,
                                                       p->base(), rhs.row(0));
   for (std::size_t i = 0; i < order1.size(); ++i)
-    SplinePath::timeFreeBasisFunctionDerivative(order1[i], 0,
+    p->basisFunctionDerivative(order1[i], 0,
                                                 coeffs.row(i + 1).transpose());
   rhs.middleRows(1, order1.size()).transpose() = derivatives1;
 
   size_type row = 1 + order1.size();
-  SplinePath::timeFreeBasisFunctionDerivative(0, 1,
+  p->basisFunctionDerivative(0, 1,
                                               coeffs.row(row).transpose());
   pinocchio::difference<pinocchio::RnxSOnLieGroupMap>(device_.lock(), q2,
                                                       p->base(), rhs.row(row));
   ++row;
   for (std::size_t i = 0; i < order2.size(); ++i)
-    SplinePath::timeFreeBasisFunctionDerivative(
+    p->basisFunctionDerivative(
         order2[i], 1, coeffs.row(i + row).transpose());
   rhs.middleRows(row, order2.size()).transpose() = derivatives2;
 
