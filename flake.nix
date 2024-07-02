0dfd067c6de596a1624a3d937efacb9e903a8ddf
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-02 11:13:58 +0200

    nix: update after v5.1.0

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     hpp-constraints = {
-      url = "github:humanoid-path-planner/hpp-constraints/release/5.1.0";
+      url = "github:humanoid-path-planner/hpp-constraints";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
